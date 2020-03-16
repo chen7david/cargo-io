5a02ba18248234f4878d496c95a3de6d7e9f2fe6
David <chen7david@me.com>
2020-03-16 22:37:08 +0800

    update

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const supported_lang = ['en', 'zh']
-const supported_states = ['info', 'warning', 'error', 'validation']
-
+const { timestamp, randInt } = require('funx-js')
 class Cargo {
 
     constructor(){
-        this.id = Math.floor((Math.random() * 9000) + 1000)
-        this.createdAt = new Date().toLocaleString()
+        this.serial = randInt(5)
+        this.createdAt = timestamp()
         this.isCargo = true
     }
 
-    detailsTo(details){
+    details(details){
         this.details = details
         return this
     }
 
-    payloadTo(payload){
+    payload(payload){
         this.payload = payload
+        return this
+    }
+
+    status(status){
+        this.status = status
+        return this
     }
 
     directive(directive){
@@ -23,65 +27,9 @@ class Cargo {
         this.directives.push(directive)
         return this
     }
-
-    static loadNotifications(Notification){
-        this.notification = Notification
-        return this
-    }
-
-    message(code, options){
-
-    }
 }
 
-class Notification {
-    constructor(code, options = null){
-        let notification = null
-        if(this.store())
-            notification = this.store().notifications.find(el => el.name == code)
-
-        if(options) {
-            const { lang, data } = options
-            if(lang) this.langTo(lang)
-        }
-        
-        this.status = notification ? notification.status : 200
-        this.code = code
-        this.state = notification ? notification.state : 'unknown'
-        this.lang = 'en'
-        this.messages = []
-    }
-
-    static loadStore(store){
-        this.store = store
-        return this
-    }
-
-    static loadErrorMutator(mutator){
-        if(!this.errorMutators) this.errorMutators = []
-        this.errorMutators.push(mutator)
-        return this
-    }
-
-    errorMutators(){
-        return this.constructor.errorMutators ?
-            this.constructor.errorMutators : []
-    }
-
-    store(){
-        return this.constructor.store ? 
-        this.constructor.store : null
-    }
-
-    langTo(lang){
-        if(supported_lang.includes(lang)) this.lang = lang
-        return this
-    }
-}
-
-module.exports = {
-    Cargo, Notification
-}
+module.exports = { Cargo }
 
 
 
